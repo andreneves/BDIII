19cbdaa1063e77d92817c6259b19aae3c72f218c
andreneves <andr@andr.com.br>
2020-02-10 15:59:16 -0300

    d

--- BDIIIA01.sql
+++ BDIIIA01.sql
@@ -6,12 +6,17 @@ nome varchar (50),
 idade int (3)
 );
 
+-- insert
 INSERT INTO alunos (nome, idade) values ('joao', null);
 
+-- selec
 select * from alunos;
 
+-- is null e is not null
 select nome, idade from alunos WHERE idade IS NULL; 
 
 select nome, idade from alunos WHERE idade IS NOT NULL; 
 
+-- distinct
 select distinct nome from alunos
+
