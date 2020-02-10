02cb2eb0faf3cb30e34935517dfcbd7e6039581f
andreneves <andr@andr.com.br>
2020-02-10 16:09:27 -0300

    c

--- BDIIIA01.sql
+++ BDIIIA01.sql
@@ -1,24 +1,26 @@
+-- create database
 CREATE DATABASE bd3;
 
+-- create table
 CREATE TABLE alunos (
-id INT AUTO_INCREMENT primary key,
-nome varchar (50),
-idade int (3)
+	id INT AUTO_INCREMENT primary key,
+	nome varchar (50),
+	idade int (3)
 );
 
 -- insert
-INSERT INTO alunos (nome, idade) values ('joao', null);
+insert into alunos (nome, idade) values ('joao', null);
 
 -- select
 select * from alunos;
 
 -- is null e is not null
-select nome, idade from alunos WHERE idade IS NULL; 
+select nome, idade from alunos where idade is null;
 
-select nome, idade from alunos WHERE idade IS NOT NULL; 
+select nome, idade from alunos where idade is not null; 
 
 -- distinct
 select distinct nome from alunos;
 
 -- order by
-select * from alunos ORDER BY nome ASC;
+select * from alunos order by nome asc;
