2f199ecd785beddef40f1bcca7fbca2229f8d2cb
andreneves <andr@andr.com.br>
2020-02-10 15:47:06 -0300

    atualizacao

---
+++ BDIIIA01.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE bd3;
+
+CREATE TABLE alunos (
+id INT AUTO_INCREMENT primary key,
+nome varchar (50),
+idade int (3)
+);
+
+INSERT INTO alunos (nome, idade) values ('joao', null);
+
+select * from alunos;
+
+select nome, idade from alunos WHERE idade IS NULL; 
+
+select nome, idade from alunos WHERE idade IS NOT NULL; 
+
