c1967e146506b165abff4e8dab8de66e5193a1b8
andreneves <andr@andr.com.br>
2020-03-04 19:25:48 -0300

    Update BDIIIA01.sql

--- BDIIIA01.sql
+++ BDIIIA01.sql
@@ -7,16 +7,16 @@ CREATE DATABASE bd3;
 CREATE TABLE alunos (
 	id INT AUTO_INCREMENT primary key,
 	nome varchar (50),
-	idade int (3)
-    nascimento date DEFAULT NULL,
+	idade int (3),
+    nascimento date DEFAULT NULL
 );
 
 -- insert
-insert into alunos (nome, idade) values ('leo', 10, '2010-03-04');
-insert into alunos (nome, idade) values ('juju', 11, '2009-10-08');
-insert into alunos (nome, idade) values ('fred', 13, '2007-05-12');
-insert into alunos (nome, idade) values ('joao', null, null);
-insert into alunos (nome, idade) values ('joao', 14, '2006-07-05');
+insert into alunos (nome, idade, nascimento) values ('leo', 10, '2010-03-04');
+insert into alunos (nome, idade, nascimento) values ('juju', 11, '2009-10-08');
+insert into alunos (nome, idade, nascimento) values ('fred', 13, '2007-05-12');
+insert into alunos (nome, idade, nascimento) values ('joao', null, null);
+insert into alunos (nome, idade, nascimento) values ('joao', 14, '2006-07-05');
 
 -- select
 select * 
