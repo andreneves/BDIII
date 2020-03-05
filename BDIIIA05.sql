46662885663328d7cf3ca93fb82776ca7694f59e
andreneves <andr@andr.com.br>
2020-03-05 16:40:07 -0300

    rr

--- BDIIIA05.sql
+++ BDIIIA05.sql
@@ -23,56 +23,56 @@ insert into alunos (nome, idade, nascimento) values ('joao', 14, '2006-07-05');
 
 -------------------
 
--- Utilizando os dados acima
+-- Utilizando a estrutura acima faça as seguintes consultas:
 
 
--- Selecione todos os registros da tabela alunos
+-- 1) Selecione todos os registros da tabela alunos
 
 
--- Retorne somente o nome e a idade das linhas que não tenham a idade nula
+-- 2) Retorne somente o nome e a idade das linhas que não tenham a idade nula
 
 
--- Retorne todos os registros em ordem decrescente de nome
+-- 3) Retorne todos os registros em ordem decrescente de nome
 
 
--- Retorne o nome e a idade de somente dois registros
+-- 4) Retorne o nome e a idade de somente dois registros
 
 
--- Atualize o nome do registro com id = 1 para "Samuel"
+-- 5) Atualize o nome do registro com id = 1 para "Samuel"
 
 
--- Exclua o registro com id = 3
+-- 6) Exclua o registro com id = 3
 
 
--- Selecione os registros que tenham o nome = 'Samuel' ou idade = 14
+-- 7) Selecione os registros que tenham o nome = 'Samuel' ou idade = 14
 
 
--- Selecione os registros que tenham o nome = 'joao' e idade = 14
+-- 8) Selecione os registros que tenham o nome = 'joao' e idade = 14
 
 
--- Selecione os registros que não possuem o nome = 'joao'
+-- 9) Selecione os registros que não possuem o nome = 'joao'
 
 
--- Conte todas as idades da tabela
+-- 10) Conte todas as idades da tabela
 
 
--- Recupere a média da idade dos alunos
+-- 11) Recupere a média da idade dos alunos
 
 
--- Some a idade dos alunos
+-- 12) Some a idade dos alunos
 
 
--- Selecione todos os registros que tenham em algum lugar do nome a string 'o'
+-- 13) Selecione todos os registros que tenham em algum lugar do nome a string 'o'
 
 
--- Selecione os registro com idade entre 10 e 12 anos
+-- 14) Selecione os registro com idade entre 10 e 12 anos
 
 
--- Selecione os registro com idade fora da faixa de 10 e 12 anos
+-- 15) Selecione os registro com idade fora da faixa de 10 e 12 anos
 
 
--- Selecione os alunos com data de nascimento entre '2007-05-12' e '2010-03-04'
+-- 16) Selecione os alunos com data de nascimento entre '2007-05-12' e '2010-03-04'
 
 
--- Retorne o nome usando o alias nome_do_cliente
+-- 17) Retorne o nome usando o alias nome_do_cliente
 
