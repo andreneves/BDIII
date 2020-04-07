c8d0441c66d42706acd53da9c630e9263651d421
andreneves <andr@andr.com.br>
2020-04-06 23:11:42 -0300

    update

--- BDIIIA02.sql
+++ BDIIIA02.sql
@@ -21,24 +21,27 @@ insert into alunos (nome, idade, nascimento) values ('fred', 13, '2007-05-12');
 insert into alunos (nome, idade, nascimento) values ('joao', null, null);
 insert into alunos (nome, idade, nascimento) values ('joao', 14, '2006-07-05');
 
--- update
+
+select * from alunos;
+
+-- atualizar nome para Elias e idade para 40 do registro com id = 1
 UPDATE alunos
 SET nome = "Elias", idade = 20
 WHERE id = 1;
 
--- delete
+-- excluir o registro com id = 1 da tabela alunos
 DELETE FROM alunos 
 WHERE id = 1;
 
--- and
+-- selecione os registro com nome de joao e idade igual a 14
 SELECT * FROM alunos
 WHERE nome='joao' AND idade=14;
 
--- or
+-- selecionar todos os registros que tenham o nome igual a joao ou idade igual a 11
 SELECT * FROM alunos
 WHERE nome='joao' OR idade=10;
 
--- not
+-- selecionar todos os registros que não tenham o nome igual a joao
 SELECT * FROM alunos
 WHERE NOT nome='joao';
 
