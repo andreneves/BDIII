a96e8afdf8849f76f687fe68ea40a0b06f99f1e5
andreneves <andr@andr.com.br>
2020-05-27 19:11:57 -0300

    Create BDIIIA06.sql

---
+++ BDIIIA06.sql
@@ -0,0 +1,46 @@
+-- BDIIIA06
+
+DROP DATABASE bd3;
+CREATE DATABASE bd3;
+
+USE bd3;
+DROP TABLE IF EXISTS alunos;
+
+-- create table
+CREATE TABLE `categorias` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `produtos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(80) NOT NULL,
+  `valor` float NOT NULL,
+  `categoria_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `categoria_id` (`categoria_id`),
+  CONSTRAINT `produtos_ibfk_1` FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`)
+);
+
+-- insert categorias 
+insert into categorias (nome) values ('eletronicos');
+insert into categorias (nome) values ('informatica');
+insert into categorias (nome) values ('escritorio');
+
+-- insert produtos 
+insert into produtos (nome, valor, categoria_id) values ('microondas', 100, 1);
+insert into produtos (nome, valor, categoria_id) values ('forno eletrico', 200, 1);
+insert into produtos (nome, valor, categoria_id) values ('teclado', 50, 2);
+insert into produtos (nome, valor, categoria_id) values ('mouse', 20, 2);
+insert into produtos (nome, valor, categoria_id) values ('mesa', 120, 3);
+insert into produtos (nome, valor, categoria_id) values ('cadeira', 80, 3);
+
+-- 
+select * from categorias;
+
+select * from produtos;
+
+SELECT categorias.nome, produtos.nome, produtos.valor
+FROM produtos
+INNER JOIN categorias ON produtos.categoria_id = categorias.id;
