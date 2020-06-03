83838c04a32ac4e3e278bf9930de20ceb8bce7d8
andreneves <andr@andr.com.br>
2020-06-03 19:42:22 -0300

    Create BDIIIA07.sql
    Union

---
+++ BDIIIA07.sql
@@ -0,0 +1,56 @@
+-- BDIIIA06
+
+DROP DATABASE bd3;
+CREATE DATABASE bd3;
+
+USE bd3;
+
+-- create table
+CREATE TABLE `vendas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(200) NOT NULL,
+  `cidade` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+
+CREATE TABLE `alugueis` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(200) NOT NULL,
+  `cidade` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- insert categorias 
+insert into vendas (nome, cidade) values ('venda 1', 'Rio de Janeiro');
+insert into vendas (nome, cidade) values ('venda 2', 'São Paulo');
+insert into vendas (nome, cidade) values ('venda 3', 'Rio de Janeiro');
+
+insert into alugueis (nome, cidade) values ('aluguel 1', 'São Paulo');
+insert into alugueis (nome, cidade) values ('aluguel 2', 'Belo Horizonte');
+insert into alugueis (nome, cidade) values ('aluguel 3', 'Rio de Janeiro');
+
+
+-- UNION
+-- O operador UNION é usado para combinar o conjunto de resultados de duas ou mais instruções SELECT.
+-- Devem respeitar a mesma ordem, número e tipo de colunas
+
+-- UNION vem com valores não duplicados (tipo distinct)
+SELECT cidade from vendas
+UNION
+SELECT cidade from alugueis;
+
+
+-- UNION ALL vem com valores duplicados
+SELECT cidade from vendas
+UNION ALL
+SELECT cidade from alugueis;
+
+-- UNION com  where
+SELECT cidade from vendas
+WHERE id >= 2
+UNION
+SELECT cidade from alugueis
+WHERE id >= 2
+ORDER BY cidade ASC;
+
