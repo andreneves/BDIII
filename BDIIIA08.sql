fb843cc7e3f14a402f310f1f93a80c757926e64a
andreneves <andr@andr.com.br>
2022-05-09 20:53:30 -0300

    up

--- BDIIIA08.sql
+++ BDIIIA08.sql
@@ -85,7 +85,6 @@ insert into vendas (nome, vendedor_id) values ('Venda 6', 3);
 select * from vendedores;
 select * from vendas;
 
-AQUIIIIII
 
 -- Desejo ordenar os vendedores começando pelo com maior número de vendas
 SELECT vendedores.nome, COUNT(vendas.id) AS QuantidadeDeVendas FROM vendas
