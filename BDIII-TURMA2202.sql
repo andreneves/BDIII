51347826ec24f1fe490de6cde05db1a7411f3dff
andreneves <andr@andr.com.br>
2022-08-09 20:55:54 -0300

    aula 01

---
+++ BDIII-TURMA2202.sql
@@ -0,0 +1,18 @@
+-- BDIII A01
+
+create database bd3;
+
+use bd3;
+
+create table alunos (
+	id int(11) primary key auto_increment,
+    nome varchar(255) not null,
+    nascimento date default null
+);
+
+insert into alunos (nome, nascimento) values ('leo', '2011-03-04');
+insert into alunos (nome, nascimento) values ('juju', '2010-10-08');
+insert into alunos (nome, nascimento) values ('fred', '2008-05-12');
+insert into alunos (nome, nascimento) values ('joao', null);
+insert into alunos (nome, nascimento) values ('joao', '2007-07-05');
+
